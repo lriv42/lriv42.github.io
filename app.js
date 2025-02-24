58fa26d171922bf15d03397a88f30b8ad8a2706d
Liam Rivers <lrivers42@tntech.edu>
2025-02-24 14:27:52 -0600

    Updated UI formatting

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 //const strApiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,is_day,weather_code&hourly=temperature_2m&temperature_unit=fahrenheit&precipitation_unit=inch'
 //end global variables
 
+//runs our asynchronous function on button click
 document.querySelector('#btnSelect').addEventListener('click', async function(){
+
+    //determines what city the user wants the weather for
     const strCity = document.querySelector('#cboCity').value.trim()
+
+    //variable initialization
     let lat = 0, long = 0
+    let strTemperatureIcon = ""
     //if statements to set each city lat long?
 
+    //sets the lat and long depending on which city is selected
     if(strCity !== ""){
         if(strCity === "los angeles"){
             //strDisplayCity = "Los Angeles"
@@ -39,10 +46,10 @@ document.querySelector('#btnSelect').addEventListener('click', async function(){
             long = -81.6944
         }
 
+        //using lat and long we make the api call with the proper url
         let strUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,weather_code&hourly=temperature_2m&temperature_unit=fahrenheit&precipitation_unit=inch`
-        
-        console.log(strUrl)
 
+        //getting our data
         try{
             const objResponse = await fetch(strUrl);
 
@@ -52,62 +59,97 @@ document.querySelector('#btnSelect').addEventListener('click', async function(){
 
             const objData = await objResponse.json();
 
-            
+            //setting variables with data returned by the api call
             let temperature = objData.current.temperature_2m
             let humidity = objData.current.relative_humidity_2m
             let apparentTemp = objData.current.apparent_temperature
             let weatherCode = objData.current.weather_code
 
+            //sets the top icon depending on the weather code
             if(weatherCode === 0){
-                strIcon = "bi bi-sun"
+                strWeatherIcon = "bi bi-sun"
             } else if(weatherCode >=1 &&  weatherCode<= 3){
-                strIcon = "bi bi-cloud-sun"
+                strWeatherIcon = "bi bi-cloud-sun"
             } else if(weatherCode === 45 || weatherCode === 48){
-                strIcon = "bi bi-cloud-fog"
+                strWeatherIcon = "bi bi-cloud-fog"
             } else if(weatherCode >= 51 && weatherCode <= 55){
-                strIcon = "bi bi-cloud-rain"
+                strWeatherIcon = "bi bi-cloud-rain"
             } else if(weatherCode === 56 || weatherCode === 57){
-                strIcon = "bi bi-cloud-sleet"
+                strWeatherIcon = "bi bi-cloud-sleet"
             } else if(weatherCode >= 61 && weatherCode <= 65){
-                strIcon = "bi bi-cloud-rain-heavy"
+                strWeatherIcon = "bi bi-cloud-rain-heavy"
             } else if(weatherCode === 66 || weatherCode === 67){
-                strIcon = "bi bi-cloud-sleet-fill"
+                strWeatherIcon = "bi bi-cloud-sleet-fill"
             } else if(weatherCode >= 71 && weatherCode <= 75){
-                strIcon = "bi bi-cloud-snow"
+                strWeatherIcon = "bi bi-cloud-snow"
             } else if(weatherCode === 77){
-                strIcon = "bi bi-snow2"
+                strWeatherIcon = "bi bi-snow2"
             } else if(weatherCode >= 80 && weatherCode <= 82){
-                strIcon = "bi bi-cloud-rain-heavy"
+                strWeatherIcon = "bi bi-cloud-rain-heavy"
             } else if(weatherCode === 85 || weatherCode === 86){
-                strIcon = "bi bi-cloud-snow"
+                strWeatherIcon = "bi bi-cloud-snow"
             } else if(weatherCode === 95){
-                strIcon = "bi bi-cloud-lightning-rain"
+                strWeatherIcon = "bi bi-cloud-lightning-rain"
             } else if(weatherCode === 96 || weatherCode === 99){
-                strIcon = "bi bi-cloud-lightning-rain"
+                strWeatherIcon = "bi bi-cloud-lightning-rain"
+            }
+
+            //sets the temperature icon
+            if(temperature >= 80){
+                 strTemperatureIcon += "bi bi-thermometer-high"
+            } else if(temperature >=40 && temperature <= 79){
+                 strTemperatureIcon += "bi bi-thermometer-half"
+            } else{
+                 strTemperatureIcon += "bi bi-thermometer"
+            }
+
+            //sets the apparent temperature icon
+            if(apparentTemp >= 80){
+                strApparentIcon = "bi bi-emoji-sunglasses"
+            } else if(apparentTemp >=40 && apparentTemp <= 79){
+                strApparentIcon = "bi bi-emoji-smile"
+            } else{
+                strApparentIcon = "bi bi-emoji-grimace"
+            }
+
+            //sets the humidity icon
+            if(humidity <= 20){
+                strHumidityIcon = "bi bi-droplet"
+            } else if(humidity >= 21 && humidity <= 55){
+                strHumidityIcon = "bi bi-droplet-half"
+            } else{
+                strHumidityIcon = "bi bi-droplet-fill"
             }
 
             
 
+        //using innerHTML we replace everything inside each respective div
+        //for example the divIcon will update to contain the weather icon
         document.querySelector('#divIcon').innerHTML = `
-        <i class="${strIcon}" style="font-size: 4rem; "></i>`
+        <i class="${strWeatherIcon}" style="font-size: 5rem; "></i>`
 
         document.querySelector("#divTemp").innerHTML = `
+        <i class="${strTemperatureIcon}" style="font-size: 3rem; "></i>
         <p>Temperature: ${temperature} Farenheit</p>`
         
         document.querySelector('#divApparent').innerHTML = `
+        <i class="${strApparentIcon}" style="font-size: 3rem; "></i>
         <p>Feels like: ${apparentTemp}</p>`
         
         document.querySelector('#divHumidity').innerHTML = `
+        <i class="${strHumidityIcon}" style="font-size: 3rem; "></i>
         <p>Humidity: ${humidity}%</p>`
             
+
     } catch(objError){
         console.log('Error fetching objData:', objError);
         
     }
 
+    //throws up an error to tell the user to select a city if they dont
     }else{
         Swal.fire({
-            title:"Oops, please select a city",
+            title:"Oops, please select a city!",
             icon: 'error'
         })
     }
